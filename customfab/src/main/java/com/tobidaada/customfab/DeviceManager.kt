76cb9e7583355ea1ccd21369c9cbf3824e7b6d30
tobialbert <daadatobi@gmail.com>
2022-02-07 04:13:36 +0100

    Feat: Save app installation date to shared preferences. Get current device time.

---
+++ customfab/src/main/java/com/tobidaada/customfab/DeviceManager.kt
@@ -0,0 +1,48 @@
+package com.tobidaada.customfab
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DeviceManager(private val sharedPreferenceManager: ISharedPreferenceManager) {
+
+    companion object {
+        private const val INSTALLATION_DATE_KEY = "installation_time_key"
+    }
+
+    // get the current time (figure out how to convert this to a flow/observable)
+    fun getCurrentTime(): String =
+        convertDateToString(Date(), DateFormat.HH_MM_SS_DD_MMM_YYYY)
+
+    // get the installation time
+    fun getAppInstallationDate(): String {
+        val dateString = sharedPreferenceManager.getString(INSTALLATION_DATE_KEY)
+
+        val date = Date()
+        if (dateString.isEmpty()) {
+            saveAppInstallationDate(date)
+            return convertDateToString(date, DateFormat.DD_MMM_YYYY)
+        }
+
+        return dateString
+    }
+
+    // save the installation time
+    fun saveAppInstallationDate(date: Date) {
+        val dateString = convertDateToString(date, DateFormat.DD_MMM_YYYY)
+        sharedPreferenceManager.saveString(INSTALLATION_DATE_KEY, dateString)
+    }
+
+    private fun convertDateToString(date: Date, dateFormat: DateFormat): String {
+        val formatter = SimpleDateFormat(dateFormat.pattern, Locale.US)
+        return formatter.format(date)
+    }
+
+    enum class DateFormat(val pattern: String) {
+        // 05 Mar 2022
+        DD_MMM_YYYY("dd MMM yyyy"),
+
+        // 12:12:12 06 Mar 2022
+        HH_MM_SS_DD_MMM_YYYY("hh:mm:ss dd MMM yyyy")
+    }
+
+}
---
+++ customfab/src/main/java/com/tobidaada/customfab/SharedPreferenceManagerImpl.kt
@@ -0,0 +1,31 @@
+package com.tobidaada.customfab
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPreferenceManagerImpl(private val context: Context): ISharedPreferenceManager {
+
+    private val sharedPreferences: SharedPreferences by lazy {
+        context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+    }
+    private val sharedPrefsEditor: SharedPreferences.Editor by lazy {
+        sharedPreferences.edit()
+    }
+
+    companion object {
+        private const val SHARED_PREF_NAME = "customfab__shared_pref_name"
+    }
+
+    override fun saveString(key: String, value: String) {
+        sharedPrefsEditor.putString(key, value).apply()
+    }
+
+    override fun getString(key: String, default: String): String =
+        sharedPreferences.getString(key, default) ?: default
+}
+
+
+interface ISharedPreferenceManager {
+    fun saveString(key: String, value: String)
+    fun getString(key: String, default: String = ""): String
+}
