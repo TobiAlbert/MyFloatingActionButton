c11f6103b34f11101ed203f79c53faacecd22c68
tobialbert <daadatobi@gmail.com>
2022-02-07 02:32:24 +0100

    Feat: Create scaffold for `CustomDialogFragment`.

---
+++ customfab/src/main/java/com/tobidaada/customfab/ui/dialog/CustomDialogFragment.kt
@@ -0,0 +1,31 @@
+package com.tobidaada.customfab.ui.dialog
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDialogFragment
+import com.tobidaada.customfab.R
+
+class CustomDialogFragment: AppCompatDialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val context = requireContext()
+        val inflater = LayoutInflater.from(context)
+
+        val view = inflater.inflate(R.layout.dialog_custom, null, false)
+
+        return AlertDialog.Builder(context)
+            .setView(view)
+            .create()
+    }
+
+    companion object {
+        fun newInstance(): CustomDialogFragment {
+            val fragment = CustomDialogFragment()
+            val bundle = Bundle()
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+}
